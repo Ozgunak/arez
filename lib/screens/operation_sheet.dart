ebbd614c35a8a86c4270fcb2e34a27692d8535e4
Özgün Aksoy <zgnaksoy@gmail.com>
2023-05-16 20:02:12 -0700

    add operations with time and price

--- lib/screens/operation_sheet.dart
+++ lib/screens/operation_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:arez/models/operation_model.dart';
+
+import '../models/operation_model.dart';
 
 class OperationBottomSheet extends StatefulWidget {
   @override
@@ -15,28 +16,105 @@ class _OperationBottomSheetState extends State<OperationBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
+    int totalOperations = operations.where((op) => op.selected).length;
+    Duration totalTime = totalOperations > 0
+        ? operations
+            .where((op) => op.selected)
+            .map((op) => op.time)
+            .reduce((value, element) => value + element)
+        : Duration.zero;
+    double totalPrice = totalOperations > 0
+        ? operations
+            .where((op) => op.selected)
+            .map((op) => op.price)
+            .reduce((value, element) => value + element)
+        : 0.0;
+
     return Container(
       height: MediaQuery.of(context).size.height / 2,
-      child: ListView.builder(
-        itemCount: operations.length,
-        itemBuilder: (context, index) {
-          return Card(
-            elevation: 4.0,
-            margin: EdgeInsets.all(8.0),
-            child: ListTile(
-              title: Text(
-                  '${operations[index].name} ${operations[index].time.inMinutes} min ${operations[index].price} CAD'),
-              trailing: operations[index].selected
-                  ? Icon(Icons.check_box, color: Colors.green)
-                  : Icon(Icons.check_box_outline_blank),
-              onTap: () {
-                setState(() {
-                  operations[index].selected = !operations[index].selected;
-                });
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RichText(
+              text: TextSpan(
+                text: 'Total Operations: ',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '$totalOperations\n',
+                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                  ),
+                  TextSpan(
+                    text: 'Total Time: ',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                  TextSpan(
+                    text: '${totalTime.inMinutes} min\n',
+                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                  ),
+                  TextSpan(
+                    text: 'Total Price: ',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                  TextSpan(
+                    text: '$totalPrice CAD',
+                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ElevatedButton(
+            child: Text('Confirm'),
+            onPressed: totalOperations > 0
+                ? () {
+                    // Implement your action on confirm button press
+                  }
+                : null,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: operations.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 4.0,
+                  margin: EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: RichText(
+                      text: TextSpan(
+                        text: '${operations[index].name} ',
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '${operations[index].time.inMinutes} min ',
+                            style: TextStyle(color: Colors.grey, fontSize: 16),
+                          ),
+                          TextSpan(
+                            text: '${operations[index].price} CAD',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                    trailing: operations[index].selected
+                        ? Icon(Icons.check_box, color: Colors.green)
+                        : Icon(Icons.check_box_outline_blank),
+                    onTap: () {
+                      setState(() {
+                        operations[index].selected =
+                            !operations[index].selected;
+                      });
+                    },
+                  ),
+                );
               },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
